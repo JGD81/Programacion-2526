55a21d9dad7a3406fbe5e2caff39ad9c8a83a120
Jose Garcia Diaz <jgarciadiaz1981@gmail.com>
2025-10-01 19:51:55 +0200

    Ejercicios 1/10

---
+++ UNIDAD1/EjerciciosEntrega.java
@@ -0,0 +1,9 @@
+package UNIDAD1;
+
+public class EjerciciosEntrega {
+    public static void main(String args[]) {
+        int numero = 23;
+        String direccion = "Calle perico";
+        System.out.println("Tu dirección es " + direccion + " número " + numero);
+    }
+}
