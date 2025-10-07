a38c4a8656ca6d99b58f870d64c90b57884aadcc
Jose García <jgarciadiaz1981@gmail.com>
2025-10-07 13:01:06 +0200

    edadv2

--- UNIDAD2/EdadMedia.java
+++ UNIDAD2/EdadMedia.java
@@ -2,24 +2,42 @@ package UNIDAD2;
 
 public class EdadMedia {
     public static void main(String[] args) {
-
+        /*
+         * Ejerciciov2: leer las edades de 10 personas, que deben de estar entre 18 y 65
+         * años
+         * se debe sacar por pantalla la edad de cada persona y al final la edad media
+         * del grupo
+         */
         int sumaEdad = 0;
         int i = 1;
         int edad = 0;
+        int cantidadPersonas = 0;
+        String mensaje = "";
 
         // Quiero que se repita 10 veces
         while (i <= 10) {
-            // Genero la edad aleaotriamente
-            edad = 18 + (int) (Math.random() * 48);
-            // Mostramos la edad de la persona
-            System.out.println("La edad de la persona " + i + "es: " + edad);
-            // Pasamos a la siguiente persona
+            // Borramos el mensaje
+            mensaje = "";
+            // Genero la edad entre 1 y 100
+            edad = (int) (Math.random() * 100) + 1;
+            if (edad < 18) {
+                mensaje = "La persona es menor de edad";
+            } else if (edad > 65) {
+                mensaje = "Esá jubilado";
+            } else {
+
+                // Pasamos a la siguiente persona
+                sumaEdad = sumaEdad + edad;
+                cantidadPersonas = cantidadPersonas + 1;
+            }
+            // Mostramos la edad del personaje
+            mensaje = "La edad de la persona " + i + " es " + edad + ". " + mensaje;
             i++;
-            sumaEdad = sumaEdad + edad;
         }
         // Calculamos la media
-        sumaEdad = sumaEdad / 10;
-        System.out.println("La media de edad es: " + sumaEdad);
-
+        if (cantidadPersonas != 0) {
+            sumaEdad = sumaEdad / cantidadPersonas;
+            System.out.println("La media de edad es: " + sumaEdad);
+        }
     }
 }
