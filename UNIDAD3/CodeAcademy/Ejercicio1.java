6d631c57943b75d81784f3ca59a92efa9fd764d3
JGD81 <garcia.diaz.jose@outlook.es>
2026-01-09 10:13:45 +0100

    8Enero

---
+++ UNIDAD3/CodeAcademy/Ejercicio1.java
@@ -0,0 +1,19 @@
+package UNIDAD3.CodeAcademy;
+
+/**
+ * Escribe tu primer hello world!
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Diferencia entre println y print
+        //Salto de línea
+        System.out.println("Let's play hide and seek.");
+        //Continúa en la misma línea
+        System.out.print("Three...");
+        System.out.print("Two...");
+        //Salto de línea
+        System.out.println("One...");
+        System.out.println("Ready or not, here I come");
+    }
+}
