467a259a0a1cf2c4f3435860a902ee3def9a6258
JGD81 <garcia.diaz.jose@outlook.es>
2026-01-14 18:25:47 +0100

    14Enero_2

--- UNIDAD3/EjercicioEntrega/Artista.java
+++ UNIDAD3/EjercicioEntrega/Artista.java
@@ -1,6 +1,7 @@
 package UNIDAD3.EjercicioEntrega;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Artista {
     //Variables miembro
@@ -118,7 +119,7 @@ public class Artista {
 
     //Métodos
     /*
-    Comprueba:
+    tocarInstrumento Comprueba:
     - Que el instrumento sea compatible con el género de artista
     - Que el instrumento no esté ya en uso
     - Si la energía del artista < 20 (no puede tocar: return false)
@@ -147,6 +148,123 @@ public class Artista {
         this.setEnergia(this.energia - 15);
         return true;
    }
+   
+   /*
+   dejarInstrumento realiza:
+   - Busca en la lista un instrumento con el nombre introducido
+        - Si lo encuentra: lo marca como no en uso, lo elimina de la lista,
+        recupera 10 de energía y lo devuelve
+   */
+   public Instrumento dejarInstrumento(String nombreInstrumento){
+        // Recorremos la lista de instrumentos con un for clásico,
+        // ya que permite eliminar elementos sin problemas
+        for(int i = 0; i < instrumentos.size(); i++){
+            //Obtenermos el instrumento en la posición actual
+            Instrumento inst = instrumentos.get(i);
+            //Comprobamos si el nombre del instrumento coincide con el recibido
+            if(inst.getNombre().equals(nombreInstrumento)){
+                //Marcamos el instrumento como no en uso
+                inst.setEnUso(false);
+                //Eliminamos el instrumento de la lista del artista
+                instrumentos.remove(i);
+                //El artista recupera 10 puntos de energía (usamos setter para validaciones)
+                this.setEnergia(this.energia + 10);
+                //Se devuelve el instrumento retirado
+                return inst;
+            }
+        }    
+        //Si no encuentra instrumento con ese nombre, devuelve null
+        return null;
+   }
+
+   /*
+   subirFama realiza
+   - Incrementa el nivel en 1
+   - Incrementa la energía en 20 (sin superar MAXIMA)
+   - Añade 1000 seguidores
+   */
+  public void subirFama(){
+    this.setNivel(this.nivel + 1);
+    this.setEnergia(this.energia + 20);
+    this.setSeguidores(this.seguidores + 1000);
+  }
+
+  /*
+  darConcierto realiza:
+  - Por cada 10 asistentes, gana un seguidor
+  - Resta 30 energía
+  - Si los seguidores >= 10000, llama a subirFama()
+  */
+  public void darConcierto(int asistentes){
+    //Como asistentes es int, la división da un resultado entero
+    //omitiendo los decimales
+    int seguidoresGanados = asistentes / 10;
+    //Se resta 30 de energía
+    this.setEnergia(this.energia - 30);
+    //Se suman lo seguidores ganados
+    this.setSeguidores(this.seguidores + seguidoresGanados);
+    //Se sube la fama si llega o supera los 10000 seguidores
+    if(this.seguidores >= 10000){
+        //Llamamos un a un método de la misma clase (this)
+        this.subirFama();
+    }
+  }
+  //Devolver una lista con instrumentos de rareza ENCONTRADO_EN_MARTE
+  //Si no existen, se devuelve una lista vacía.
+  public ArrayList<Instrumento> obtenerInstrumentosMarcianos() {
+    //Creamos un stream a partir de la lista de instrumentos del artista.
+    //Un stream nos permite recorrer la colección de forma funcional.
+    return new ArrayList<>(instrumentos.stream()
+    //filter recorre internamente todos los instrumentos de la lista.
+    //La variable 'inst' representa CADA instrumento de la lista,
+    //uno por uno, en cada iteración del stream.
+    .filter(inst -> inst.getRareza() == Instrumento.ENCONTRADO_EN_MARTE)
+    //collect convierte el resultado del stream en una lista.
+    //Collectors.toList() devuelve un List, no un ArrayList.
+    .collect(Collectors.toList()));
+  }
+
+  //Usando streams, sumar el volumen de todos los instrumentos
+  //Se devuelve: (volumen total) + (nivel * 50) + (energia * 2)
+  public int calcularPoderEscenico(){
+    int volumenTotal = instrumentos.stream()
+    //Convertimos cada instrumento en su volumen y lo sumamos
+    .mapToInt(inst -> inst.getVolumen()).sum();
+    //Se aplica la fórmula del poder escénico
+    return volumenTotal + (this.getNivel() * 50) + (getEnergia()*2);
+  }
+
+  //Método auxiliar para convertir el género a texto
+  private String generoATexto(){
+    switch (generoMusical) {
+        case DEATH_METAL_COSMICO:
+            return "Death Metal Cósmico";
+        case REGGAETON_MARCIANO:
+            return "Reggaeton Marciano";
+        case JAZZ_CUANTICO: 
+            return "Jazz Cuántico";
+        case POLKA_INTERGALACTICA: 
+            return "Polka Intergaláctica";
+        case TRAP_ESPACIAL:
+            return "Trap Espacial";
+        default:
+            return "Desconocido";
+    }
+  }
+
+
+  @Override
+  public String toString(){
+    return "====================ARTISTA====================\n"
+    + "Nombre Artístico: " + this.nombreArtistico + "\n"
+    + "Género musical: " + generoATexto() + "\n"
+    + "Nivel: " + nivel + "\n"
+    + "Energía: " + energia + "\n"
+    + "Seguidores: " + seguidores + "\n"
+    + "En gira: " + (enGira ? "Sí" : "No") + "\n"
+    + "Número de instrumentos: " + instrumentos.size();
+
+  }
 
 
 }
---
+++ UNIDAD3/EjercicioEntrega/Escenario.java
@@ -0,0 +1,5 @@
+package UNIDAD3.EjercicioEntrega;
+
+public class Escenario {
+    
+}
