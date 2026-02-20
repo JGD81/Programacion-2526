27d7f6bd62e04149a54e63ec57905ab468b149bf
JGD81 <jgarciadiaz1981@gmail.com>
2026-02-20 14:43:03 +0100

    20Febrero

--- EJERCICIOUD4/guerra_jardin/src/main/java/com/ejercicioud4/model/CrudModel.java
+++ EJERCICIOUD4/guerra_jardin/src/main/java/com/ejercicioud4/model/CrudModel.java
@@ -31,25 +31,32 @@ public abstract class CrudModel {
     //Método auxiliar
     protected List<Map<String, Object>> executeQuery(String sql, Object... params){
 
-        //Creamos la lista
+        //Creamos la lista de resultados
         List<Map<String,Object>> lista = new ArrayList<>();
 
+        //Try con recursos (el PreparedStatement se cerrará automáticamente)
         try (PreparedStatement stmt = con.prepareStatement(sql)){
 
+            //Asignación de parámetros
             for(int i= 0; i < params.length; i++){
                 stmt.setObject( i + 1, params[i]);
             }
-
+            
+            //Ejecutar la consulta
             ResultSet rs = stmt.executeQuery();
-
+            
+            //Se recorren los resultados 
             while (rs.next()) {
 
+                //Crear un registro
                 Map<String, Object> registro = new HashMap<>();
 
+                //Cargar las columnas dinámicamente
                 for (String col : columns){
                     registro.put(col, rs.getObject(col));
                 }
 
+                //Añade el registro a la lista
                 lista.add(registro);
                 
             }
@@ -58,7 +65,32 @@ public abstract class CrudModel {
             e.printStackTrace();
         }
 
+        //Se devuelve la lista
         return lista;
+    }
+
+    //Método Obtener todos los registros de la tabla
+    public List<Map<String,Object>> findAll(){
+        
+        //Creamos la consulta
+        String sql = "SELECT * FROM " + table;
+
+        //Devolvemos el resultado de la consulta usando el método auxilar
+        return executeQuery(sql);
+    }
+
+    //Método buscar por ID
+    public Map<String,Object> findById(Object id){
+
+        //Se construye la consulta
+        String sql = "SELECT * FROM " + table + "WHERE " + idColumn + " = ?";
+
+        //Ejecutamos la consulta
+        List<Map<String,Object>> lista = executeQuery(sql, id);
+        
+        //Devuelve true si la lista no tiene elementos
+        //o el elemento obtenido
+        return lista.isEmpty() ? null : lista.get(0);
     }
 
 
